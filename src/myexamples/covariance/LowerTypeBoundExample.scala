93d5e7256736513b4f526309849a6972a5b2607a
Tushar Chokshi <tushar239@gmail.com>
2016-12-20 14:45:32 -0800

    more

--- src/myexamples/covariance/LowerTypeBoundExample.scala
+++ src/myexamples/covariance/LowerTypeBoundExample.scala
@@ -24,7 +24,28 @@ object LowerTypeBoundExample extends App {
   // Unfortunately, this type is invariant in the type parameter of class ListNode; i.e. ListNode[String] is not a subtype of ListNode[Any].
   // With the help of variance annotations we can express such a subtype semantics:
 
-  class ListNode[+T](h: T, t: ListNode[T]) {// +T means a type variable T should be used at Covariant position. It means you should pass U >: T to it.
+  /*
+  http://stackoverflow.com/questions/14584830/covariant-type-t-occurs-in-contravariant-position
+  
+  ListNode[+T] means if U <: T (U is a child class of T), then ListNode[U] <: ListNode[T]
+
+        Any
+        |
+      -----------
+      |         |
+     String     Emp
+        |
+     -------
+    |       |
+    S1       S2
+
+  ListNode[+T] means if it is valid to do Any any = new String(), then it should be valid to do ListNode[Any] anyListNode = new ListNode[String]
+  So, now you should be able to do anyListNode.prepend(new Emp(...)). To make that possible, you need to have 'def prepend[U >: T]'. It should be able to accept any child class object of Any class or an object of Any class.
+  If you do 'def prepend[T]' ,then it won't be possible. So, Scala doesn't allow that. It will give an error 'error: covariant type T occurs in contravariant position in type T of value elem'
+
+  */
+  class ListNode[+T](h: T, t: ListNode[T]) {
+    // +T means a type variable T should be used at Covariant position. It means you should pass U >: T to it.
     def head: T = h
 
     def tail: ListNode[T] = t
@@ -35,14 +56,22 @@ object LowerTypeBoundExample extends App {
     //def prepend(elem: T): ListNode[T] = new ListNode(elem, this)
     def prepend[U >: T](elem: U): ListNode[U] = new ListNode(elem, this)
 
+
+    override def toString = s"ListNode($head,$tail)"
   }
 
+  case class Emp(val name: String)
 
-  val empty: ListNode[Null] = new ListNode(null, null)
+  // creating ListNode[String]
+  var stringListNode: ListNode[String] = new ListNode[String]("abc", null)
+  // adding another string value and creating a new ListNode[String]
+  stringListNode = stringListNode.prepend("def")
 
-  val strList: ListNode[String] = empty.prepend("hello").prepend("world")
+  // As per the rule of +T (here, +String), you should be able to assign ListNode[String] to ListNod[Any] because you can assign String to Any.
+  var anyListNode: ListNode[Any] = stringListNode;
+  // adding Emp object to ListNode[Any]. This will be allowed only with 'def prepend[U >: T]'. 'def prepend[T]' will not work.
+  anyListNode = anyListNode.prepend(Emp("Tushar")) // ListNode((Emp(Tushar),ListNode(def))) --- what you just did is that you added Emp object to a ListNode[String]
 
-  val anyList: ListNode[Any] = strList.prepend(12345)
-
+  println(anyListNode) // ListNode(Emp(Tushar),ListNode(def,ListNode(abc,null)))
 
 }
